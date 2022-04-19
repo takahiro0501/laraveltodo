fe6a22e29d09c5cac0a13c69b96068c0d8013208
takahiro <h.takai0501@gmail.com>
2022-04-19 11:32:08 +0900

    csschange

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TodoList</title>
-    <link rel="stylesheet" href="{{ asset('/css/todo.css') }}">
+    <link rel="stylesheet" href="css/todo.css">
   </head>
   <body>
   <div class="content">
